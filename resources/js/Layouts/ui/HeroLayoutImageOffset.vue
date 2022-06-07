f41ca6ab00413ba4d7396450b131737eed9a1f6e
Amaan Ali <amaanali2002@hotmail.com>
2022-06-07 19:53:58 -0400

    extracted hero to separate layout

--- resources/js/Layouts/ui/HeroLayoutImageOffset.vue
+++ resources/js/Layouts/ui/HeroLayoutImageOffset.vue
@@ -3,23 +3,30 @@
         <div class="pt-10 bg-[#f5f7f8] sm:pt-16 lg:pt-0 lg:-mb-56">
             <div class="mx-auto lg:px-8">
                 <div
-                    class="lg:grid lg:grid-cols-2 lg:gap-8 px-6 max-w-7xl mx-auto"
+                    class="lg:grid lg:grid-cols-2 lg:gap-8 px-6 max-w-7xl mx-auto w-full"
                 >
                     <!-- Text -->
                     <div
-                        class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center"
+                        class="lg:pt-24 w-full items-end mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 text-center lg:px-0 lg:text-left lg:flex lg:items-center"
                     >
-                        <div class="lg:py-24">
+                        <div class="lg:py-24 w-full items-end">
                             <slot name="left" />
                         </div>
                     </div>
 
                     <!-- Image -->
-                    <div class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+                    <div
+                        class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative w-full"
+                    >
                         <div
-                            class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0"
+                            class="w-full mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0"
                         >
-                            <slot name="right" />
+                            <img
+                                id="heroImage"
+                                class="w-full rounded-2xl aspect-square object-cover lg:absolute lg:inset-y-0 lg:left-0 lg:aspect-square lg:mt-10 lg:max-w-none"
+                                :src="imageUrl"
+                                alt="An MSA"
+                            />
                         </div>
                     </div>
                 </div>
@@ -32,21 +39,11 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    // props: {
-    //     size: {
-    //         required: false,
-    //         type: String,
-    //         default: "base",
-    //         validator: (prop) =>
-    //             ["xs", "sm", "base", "lg", "xl"].includes(prop),
-    //     },
-    //     size: {
-    //         required: false,
-    //         type: String,
-    //         default: "base",
-    //         validator: (prop) =>
-    //             ["xs", "sm", "base", "lg", "xl"].includes(prop),
-    //     },
-    // },
+    props: {
+        imageUrl: {
+            required: true,
+            type: String,
+        },
+    },
 });
 </script>
