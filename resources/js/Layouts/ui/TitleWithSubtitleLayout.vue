5d055b40b79428d5fa90743c254c844ead5b08e8
Amaan Ali <amaanali2002@hotmail.com>
2022-06-07 22:10:35 -0400

    created for use on msa index

---
+++ resources/js/Layouts/ui/TitleWithSubtitleLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="border-b border-gray-200 py-10">
+        <page-title>
+            <slot name="title" />
+        </page-title>
+        <page-subtitle>
+            <slot name="subtitle" />
+        </page-subtitle>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import PageTitle from "@/Components/texts/PageTitle.vue";
+import PageSubtitle from "@/Components/texts/PageSubtitle.vue";
+
+export default defineComponent({
+    components: {
+        PageTitle,
+        PageSubtitle,
+    },
+});
+</script>
